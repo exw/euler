bff7592d687c5b62db89a1aeba79612637d8ec56
exw <exw@users.noreply.github.com>
2014-11-21 14:59:35 -0600

    Update 5.py

--- src/5.py
+++ src/5.py
@@ -1,42 +1,40 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+assert problem5(10) == 2520
+
 def problem5(input):
-	d = collectFactors(input)
-	answer = product(d)
+	factors = collectFactors(input)
+	print factors
+	answer = product(factors)
 	print str(answer)
 
 
-def collectFactors(i):
-	factors = newDict(i)
-	for x in xrange(i):
-		factors = newFactors(factors, getPrimes(x))
+def collectFactors(arg):
+	factors = newDict(arg)
+	for x in xrange(2,arg+1):
+		gp = getPrimes(x)
+		for i in xrange(2,x+1):
+			if gp[i] > factors[i]:
+				factors[i] = gp[i]
 	return factors
 	
 def getPrimes(i):
 	f = newDict(i)
-	r = xrange(i)
-	for x in :
+	for x in xrange(2,i+1):
 		while i % x == 0:
 			f[x] = f[x] + 1
 			i = i / x
 	return f	
-	
-def newFactors(answer,new):
-	print answer,new
-	for key,value in new:
-		print "why dont this work"
-		if answer[key] < value:
-			answer[key] = value
-	print answer
-	return answer
 
-def newDict(i): 
-	return {key: 0 for key in xrange(i)}
+def newDict(i):
+	return {key: 0 for key in xrange(2,i+1)}
 
 def product(d):
-	print "p ok"
 	answer = 1
-	for key,value in d:
+	for key,value in d.iteritems():
 		answer = answer * (key**value)
 	return answer
 
-problem5(10)
-	
+problem5(20)
