75cc2ffe8feba0efe9253fd3e6fccc7732db197d
exw <exw@openmailbox.org>
2015-01-08 16:57:48 -0500

    Solved problem 11

--- src/11.py
+++ src/11.py
@@ -1,13 +1,58 @@
-import math
-import numpy
-def primeList(upto):
-	upto = long(upto)
-	primes=numpy.arange(3,upto+1,2)
-	isprime=numpy.ones((upto-1)/2,dtype=bool)
-	for factor in primes[:int(math.sqrt(upto))]:
-		if isprime[(factor-2)/2]: isprime[(factor*3-2)/2::factor]=0
-	return numpy.insert(primes[isprime],0,2)
-    
-huge = primeList(2000000)
-answer = sum(huge)
-print answer
+txt = """08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"""
+
+raw = txt.split()
+data = []
+for i in raw:
+    data.append(int(i))
+gridLen = 20
+seq = 4
+
+def index(x,y):
+    return (x-1)*gridLen+(y-1)
+
+def findMax(x0,x1,y0,y1,offset):
+    localMax = 0
+    for x in xrange(x0,x1):
+        for y in xrange(y0,y1):
+            target = index(x,y)
+            product = 1
+            for i in xrange(seq):
+                product *= data[target + offset*i]
+            if product > localMax:
+                localMax = product
+    return localMax
+
+maxes = []
+maxes.append(findMax(1,gridLen,1,gridLen-seq+1,1))
+maxes.append(findMax(1,gridLen-seq+1,1,gridLen,gridLen))
+maxes.append(findMax(1,gridLen-seq+1,seq,gridLen,gridLen-1))
+maxes.append(findMax(1,gridLen-seq+1,1,gridLen-seq+1,gridLen+1))
+
+print maxes
+
+max = 0
+for i in maxes:
+    if i > max:
+        max = i
+
+print max
+
